ab434f4fd86cd8a8e070065bc46ac3678117b2bf
Naveen <naveen92345@gmail.com>
2024-01-07 12:11:17 +0530

    Deleted polls and sessions app

--- polls/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from polls.models import Session
-
-# Register your models here.
-
-admin.site.register(Session)
--- project1/settings.py
+++ project1/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shell_plus',
-    'polls',
     'authentications'
 ]
 
