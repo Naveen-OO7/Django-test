01f30582c5f30e97a4d2cd9f4c690791aff1c370
Naveen <naveen92345@gmail.com>
2024-01-07 13:09:20 +0530

    Added User model

--- authentications/admin.py
+++ authentications/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from authentications import models as authentication_models
+
+
+class CustomUserAdmin(UserAdmin):
+    """
+    User admin class for User model
+    """
+    fieldsets = (
+        UserAdmin.fieldsets +
+        (
+            (
+                _('Additional fields'),
+                {'fields': ('login_count', )}
+            ),
+        )
+    )
+
+
+admin.site.register(authentication_models.User, CustomUserAdmin)
--- authentications/models.py
+++ authentications/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import models as auth_models
 
-# Create your models here.
+
+class User(auth_models.AbstractUser):
+    """
+    This model is for storing user table
+    """
+
+    login_count = models.IntegerField(default=0)
